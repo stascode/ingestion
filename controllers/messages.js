d1e5146f6fcc058ccdafee9f13195b37cb310d72
Tim Park <timfpark@gmail.com>
2014-11-18 16:03:36 -0800

    add log message for relaying messages

--- controllers/messages.js
+++ controllers/messages.js
@@ -2,6 +2,8 @@ var config = require('../config')
   , utils = require('../utils');
 
 exports.create = function(req, res) {
+    console.log('relaying message');
+
     config.message_hub.send(req.user, req.body, function(err, messages) {
         if (err) return utils.handleError(res, err);
 
